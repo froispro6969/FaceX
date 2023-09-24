4150d48d6a2d01d7f89815b7a5cf602b1328e17c
Damian <froispro6969@gmail.com>
2023-09-24 16:25:39 +0200

    Design of a navbar

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
 import { Navbar } from './components/Navbar'
+import './App.css'
 
 function App() {
 
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -2,16 +2,16 @@ import { Link } from 'react-router-dom'
 
 export const Navbar = () => {
     return (
-        <div className="navbar-container">
+        <nav className="navbar-container container">
             <h1 className="navbar-logo">FaceX</h1>
             <div className="navbar-link">
-                <Link to={"/"}>Home</Link>
+                <Link to={"/"} className=''>Home</Link>
                 <Link to={"/profile"}>Profile</Link>
             </div>
             <div className='navbar-sign'>
                 <Link to={"/login"}>Sign in</Link>
                 <Link to={"/register"}>Create account</Link>
             </div>
-        </div>
+        </nav>
     )
 }
