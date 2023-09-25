1d6a75dfbc490386d1603161f9cba52afa61fead
Damian <froispro6969@gmail.com>
2023-09-25 20:05:01 +0200

    Design of a Register Page

--- src/pages/Register.tsx
+++ src/pages/Register.tsx
@@ -1,12 +1,24 @@
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faUser, faEnvelope, faLock} from '@fortawesome/free-solid-svg-icons'
+
 export const Register = () => {
     return (
         <div className="register-container">
             <div className="register">
                 <h1>Sign Up!</h1>
-                <input type="text" placeholder="Name..."/>
-                <input type="text" placeholder="Email..." />
-                <input type="text" placeholder="Password..."/>
-                <button>Register</button>
+                <div className='register-login'>
+                    <FontAwesomeIcon icon={faUser} className='userIcon' />
+                    <input type="text" placeholder="Login..."></input>
+                </div>
+                <div className='register-email'>
+                    <FontAwesomeIcon icon={faEnvelope} className='emailIcon' />
+                    <input type="text" placeholder="Email..." />
+                </div>
+                <div className='register-password'>
+                    <FontAwesomeIcon icon={faLock} className='passIcon'/>
+                    <input type="password" placeholder="Password..." />
+                </div>
+                <button className='register-button'>Register</button>
             </div>
         </div>
     )
