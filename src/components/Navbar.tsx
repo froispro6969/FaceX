c4d4fc9c109938de97b6b9150ba1898abf63fb45
Damian <froispro6969@gmail.com>
2023-09-26 18:44:05 +0200

    Added to a navbar logged user information.

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -4,19 +4,28 @@ import { useAuthState } from 'react-firebase-hooks/auth'
 
 export const Navbar = () => {
 
+    const [user] = useAuthState(auth);
 
-    
     return (
         <nav className="navbar-container container">
             <h1 className="navbar-logo">FaceX</h1>
             <div className="navbar-link">
-                <Link to={"/"} className=''>Home</Link>
+                <Link to={"/"} >Home</Link>
                 <Link to={"/profile"}>Profile</Link>
             </div>
-            <div className='navbar-sign'>
-                {}
-                <Link to={"/login"}>Sign in</Link>
-                <Link to={"/register"}>Create account</Link>
+            <div className='navbar-right'>
+                {!user ?
+                    <div className='navbar-sign'>
+                        <Link to={"/login"}>Sign in</Link>
+                        <Link to={"/register"}>Create account</Link>
+                    </div>
+                    :
+                    <div className='navbar-userInfo'>
+                    <p>{user?.email}</p>
+                    <button>Log out</button>
+                    </div>
+                }
+
             </div>
         </nav>
     )
