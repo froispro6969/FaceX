f4357a7e2509e33e56e43720ceeca21db3fdb5a5
Damian <froispro6969@gmail.com>
2023-09-26 17:35:32 +0200

    Register design updated.

--- src/pages/Register.tsx
+++ src/pages/Register.tsx
@@ -1,24 +1,31 @@
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faUser, faEnvelope, faLock} from '@fortawesome/free-solid-svg-icons'
+import { faUser, faEnvelope, faLock } from '@fortawesome/free-solid-svg-icons'
+import { useState } from 'react';
+
 
 export const Register = () => {
+
+    
+    const register = async () => {
+
+    }
+
+
     return (
         <div className="register-container">
             <div className="register">
-                <h1>Sign Up!</h1>
-                <div className='register-login'>
-                    <FontAwesomeIcon icon={faUser} className='userIcon' />
-                    <input type="text" placeholder="Login..."></input>
-                </div>
-                <div className='register-email'>
-                    <FontAwesomeIcon icon={faEnvelope} className='emailIcon' />
-                    <input type="text" placeholder="Email..." />
-                </div>
-                <div className='register-password'>
-                    <FontAwesomeIcon icon={faLock} className='passIcon'/>
-                    <input type="password" placeholder="Password..." />
-                </div>
-                <button className='register-button'>Register</button>
+                <form>
+                    <h1>Sign Up!</h1>
+                    <div className='register-email'>
+                        <FontAwesomeIcon icon={faEnvelope} className='emailIcon' />
+                        <input type="text" placeholder="Email..." />
+                    </div>
+                    <div className='register-password'>
+                        <FontAwesomeIcon icon={faLock} className='passIcon' />
+                        <input type="password" placeholder="Password..." />
+                    </div>
+                    <button className='register-button'>Register</button>
+                </form>
             </div>
         </div>
     )
