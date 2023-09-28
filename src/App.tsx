edd154b213b79adba8528c7e1d2af1b3ebb3dda1
Damian <froispro6969@gmail.com>
2023-09-28 18:58:52 +0200

    Added a home page.

--- src/App.tsx
+++ src/App.tsx
@@ -3,8 +3,10 @@ import { Navbar } from './components/Navbar'
 import './styles/App.css'
 import './styles/Navbar.css'
 import './styles/Register_and_Login.css'
+import './styles/Home.css'
 import { Register } from './pages/Register'
 import { Login } from './pages/Login'
+import { Home } from './pages/Home'
 
 function App() {
 
@@ -14,6 +16,7 @@ function App() {
       <Router>
         <Navbar />
         <Routes>
+        <Route path="/" element={<Home />}/>
           <Route path="/register" element={<Register />}/>
           <Route path="/login" element={<Login />}/>
         </Routes>
---
+++ src/pages/Home.tsx
@@ -0,0 +1,9 @@
+export const Home = () => {
+    return (
+        <div className="home-container">
+            <div className="home-page">
+                <h1>Welcome to the React App!</h1> 
+            </div>
+        </div>
+    )
+}
