74b4f0f015220e8007cc12ee0d1dbe5407f8a4a7
Damian <froispro6969@gmail.com>
2023-10-02 17:07:35 +0200

    Created an addLike functionality to a post.

--- src/components/Post.tsx
+++ src/components/Post.tsx
@@ -1,24 +1,54 @@
-import { collection, getDocs } from "firebase/firestore"
-import { db } from "../config/Firebase-config";
+import { addDoc, collection, getDocs } from "firebase/firestore"
+import { auth, db } from "../config/Firebase-config";
 import { useEffect, useState } from "react";
+import { useAuthState } from "react-firebase-hooks/auth";
 
 interface Post {
-    id: string;
+    postid: string;
     userID: string;
     username: string;
     description: string;
 }
 
+interface Likes {
+    userid: string;
+    likeID: string;
+}
+
+
 export const Post = () => {
 
+
+
     const [postsList, setPostsList] = useState<Post[]>();
+    const [likes, setLikes] = useState<Likes[] | null>(null);
+    const [user] = useAuthState(auth);
 
     const postRef = collection(db, "Posts");
+    const likeRef = collection(db, "Likes");
+
 
 
     const getPosts = async () => {
         const data = await getDocs(postRef);
-        setPostsList(data.docs.map((doc) => ({ ...doc.data(), id: doc.id })) as Post[]);
+        setPostsList(data.docs.map((doc) => ({ ...doc.data(), postid: doc.id })) as Post[]);
+    }
+
+    const addLike = async (postid: string) => {
+        try {
+            const newDoc = await addDoc(likeRef, {
+                userid: user?.uid,
+                postID: postid,
+            })
+            if(user)
+            {
+                setLikes((prev) => prev ? [...prev, { userid: user.uid, likeID: newDoc.id  }] : [{ userid: user.uid, likeID: newDoc.id }])
+            }
+    
+            }
+            catch (err) {
+                console.log(err);
+            }
     }
 
     useEffect(() => {
@@ -35,6 +65,7 @@ export const Post = () => {
                     <div className='body'>
                         <p>{post.description}</p>
                     </div>
+                    <button onClick={() => addLike(post.postid)}>&#128077;</button>
                 </div>
             ))}
         </div>
